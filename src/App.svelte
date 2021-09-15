c346bb780b1c00d0115ee8f806abd2585eb9e284
alberorana@gmail.com <alberorana@gmail.com>
2021-09-15 18:45:14 +0900

    add

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
 	export let name;
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
 </script>
 
 <main>
+	<Header />
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Footer />
 </main>
 
 <style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,5 @@
+<script>
+
+</script>
+
+<header>ヘッダー</header>
