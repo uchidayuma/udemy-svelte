49a101ac28132572d3997b33aa49f6c1498fb8a2
alberorana@gmail.com <alberorana@gmail.com>
2021-09-17 10:31:38 +0900

    wip

--- src/components/Create.svelte
+++ src/components/Create.svelte
@@ -1,5 +1,14 @@
 <script>
+  import { Slider, TextField, Button } from "smelte";
 
+  let rate = 5;
+  let body = '';
 </script>
 
-<p>Create</p>
+<h3>日記を書こう！</h3>
+<form class='p-5'>
+  <p class='mb-4'>今日の気分は{rate}点です</p>
+  <Slider class='mb-4' min="0" max="10" bind:value={rate} />
+  <TextField label="日記の本文" class="bg-white-900" bind:value={body} textarea rows="5" outlined />
+  <Button outlined>日記を保存</Button>
+</form>
