1d565e7a375567cd23c308c60a7b0a23dbccfb24
alberorana@gmail.com <alberorana@gmail.com>
2021-09-19 13:47:07 +0900

    ok

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,7 +1,8 @@
 <script>
   import { onMount, onDestroy } from "svelte";
+  import { signInWithGoogle } from '../helpers/firebase';
   import { Router, Link } from 'svelte-routing';
-  import { Button } from "smelte";
+  import { Button, ProgressCircular } from "smelte";
   import { userId } from '../store';
   import { fetch } from '../helpers/api';
   import StarRating from 'svelte-star-rating';
@@ -15,25 +16,27 @@
   })
   onDestroy( () => { unsubscribe; })
 </script>
-  
-{#await promise}
-  <p>Loading ...</p>
-{:then diaries} 
-  <Router>
-    {#each diaries as d}
-      <Link to={'/diary/' + d.id} class='flex items-center mb-6'>
-        <aside class='diary-aside'>
-          <p class='text-left'>{dayjs(d.createdAt).format('YYYY年MM月DD日')}</p>
-          <img src={ d.image ? d.image : '/dummy.jpeg'} class='diary-image' alt='diary'/>
-          <p><StarRating rating={d.rate / 2} /></p>
-        </aside>
-        <p>{d.body}</p>
-      </Link>
-    {/each}
-  </Router>
-  
-{/await}
-
+ 
+{#if !uid}
+  <Button on:click={signInWithGoogle} class='text-white-900 mt-10'>ログイン</Button>
+{:else}
+  {#await promise}
+    <p class='mt-10 flex justify-center'><ProgressCircular /></p>
+  {:then diaries} 
+    <Router>
+      {#each diaries as d}
+        <Link to={'/diary/' + d.id} class='flex items-center mb-6'>
+          <aside class='diary-aside'>
+            <p class='text-left'>{dayjs(d.createdAt).format('YYYY年MM月DD日')}</p>
+            <img src={ d.image ? d.image : '/dummy.jpeg'} class='diary-image' alt='diary'/>
+            <p><StarRating rating={d.rate / 2} /></p>
+          </aside>
+          <p>{d.body}</p>
+        </Link>
+      {/each}
+    </Router>
+  {/await}
+{/if}
 <style>
   .diary-aside {
     width: 40%;
