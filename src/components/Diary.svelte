3aa1a2855c860318df0659f7de300b92494cf840
alberorana@gmail.com <alberorana@gmail.com>
2021-09-20 11:16:00 +0900

    ok

--- src/components/Diary.svelte
+++ src/components/Diary.svelte
@@ -5,20 +5,29 @@
   import dayjs from 'dayjs';
   export let id;
   let promise = getDiary();
-  let rate, body;
+  let rate, body, image, preview;
   onMount( async() =>{
     promise = await getDiary(id);
     rate = promise.rate;
     body = promise.body;
   })
   const submit = async() =>{
-    const returnValue = await updateDiary(id, body, rate);
+    const returnValue = await updateDiary(id, body, rate, promise.image, image);
     if(returnValue){
       alert('日記の更新が完了しました。');
     }else{
       alert('更新ができませんでした。やり直しでください');
       document.location.href('/');
     }
+  }
+  const onFileSelect = (e) => {
+    let target = e.target.files[0];
+    image = target;
+    let reader = new FileReader();
+    reader.readAsDataURL(target);
+    reader.onload = e => {
+      preview = e.target.result
+    };
   }
 </script>
 
@@ -27,7 +36,13 @@
 {:then diary} 
   <h1 class='h4'>{dayjs(diary.createdAt).format('YYYY年MM月DD日')}の日記</h1>
   <form on:submit|preventDefault={submit} class='p-5'>
-    <img src={diary.image ? diary.image : '/dummy.jpeg'} alt='diary'/>
+    {#if !preview}
+      <img class='mb-4' src={diary.image ? diary.image : '/dummy.jpeg'} alt='diary'/>
+    {:else}
+      <img class='mb-4' src={preview} alt='diary'/>
+    {/if}
+    <label for='file-input' class='bg-primary-900 text-white-900 px-4 py-3 rounded block w-4/12 m-auto mb-6'>画像を選択</label>
+    <input type='file' accept="image/*" id='file-input' class='hidden' bind:this={image} on:change={(e) => onFileSelect(e)} />
     <p class='mb-4'>気分は{rate}点です</p>
     <Slider class='mb-4' min="1" max="10" bind:value={rate} />
     <TextField label="日記の本文（変更する場合は編集）" class="bg-white-900" bind:value={body} textarea rows="5" outlined />
