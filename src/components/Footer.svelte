252e2b07b1f8dbdc134e62c6b303374b437d4e85
alberorana@gmail.com <alberorana@gmail.com>
2021-09-20 16:47:44 +0900

    fix

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,8 +1,24 @@
 <script>
+  import { onDestroy } from 'svelte';
+  import { Router, Link } from 'svelte-routing';
+  import { userId } from '../store';
+  let uid;
 
+  const unsbscribe = userId.subscribe( id => {
+    uid = id;
+  })
+  onDestroy(() => unsbscribe);
 </script>
 
-<footer class='bg-secondary-900'>フッター</footer>
+<footer class='flex items-center justify-around bg-secondary-900'>
+  <Router>
+    <Link class='block mb-3' to="/">Home</Link>
+    <Link class='block mb-3' to="about">日記の効果とは？</Link>
+  {#if uid}
+    <Link class='block mb-3' to="create">日記を書く</Link>
+  {/if}
+  </Router>
+</footer>
 
 <style>
   footer{
